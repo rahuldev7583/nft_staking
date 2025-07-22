ed70a4bcda6a1af1d6f8af8688eeff8dca02efd5
rahuldev7583 <rahuldev7583@gmail.com>
2025-07-23 00:48:33 +0530

    initial commit

---
+++ programs/anchor_nft_staking/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("Gq49ENdEbCN4uQXbbd69BHLpza2aFQW5XkepyQYmtmdV");
+
+#[program]
+pub mod anchor_nft_staking {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
